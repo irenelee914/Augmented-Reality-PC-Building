a3a2d66b9497c21783cff96b992ae86d2c573de8
irenelee <irenelee914@gmail.com>
2022-01-08 20:27:05 -0500

    added two RAM steps

--- PC Building/ViewController.swift
+++ PC Building/ViewController.swift
@@ -11,19 +11,77 @@ import RealityKit
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
-    var anchor:Experience.Box!
+    var notificationTrigger: RAM.NotificationTrigger!
+    var stateController: StateController!
+    var textInstructions:UILabel!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // PopUP
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1) {
+                    let pop = Popup()
+                    self.view.addSubview(pop)
+        }
         
-        anchor = try! Experience.loadBox()
-        anchor.generateCollisionShapes(recursive: true)
+        textInstructions = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 20))
+        textInstructions.center = CGPoint(x: self.view.frame.midX, y: self.view.frame.midY+250)
+        textInstructions.textAlignment = .center
+        textInstructions.text = "I'm a test label"
+        self.view.addSubview(textInstructions)
         
-        arView.scene.anchors.append(anchor)
+        stateController = StateController()
+        updateAnchor()
     }
     
-    @IBAction func button(_ sender: Any) {
-        anchor.notifications.fanStart.post()
+    @IBAction func prevButton(_ sender: Any) {
+        if stateController.step > 1 {
+            stateController.step -= 1
+        }
+        print("\(stateController.step)")
+        updateAnchor()
     }
+
+    @IBAction func nextButton(_ sender: Any) {
+        stateController.step += 1
+        print("\(stateController.step)")
+        updateAnchor()
+    }
+    
+    func updateAnchor() -> Void {
+        if stateController.step == 1 {
+            //step 1, depress RAM levers
+            arView.scene.anchors.removeAll()
+            let anchor = try! RAM.loadRAM1()
+            anchor.generateCollisionShapes(recursive: true)
+            arView.scene.anchors.append(anchor)
+            notificationTrigger = anchor.notifications.stepRAM2
+            textInstructions.text = "Depress the white levers"
+            
+        }
+        else if stateController.step == 2 {
+            //step 2, insert RAM sticks
+            notificationTrigger.post()
+            textInstructions.text = "Place ram sticks in until click"
+            
+        }
+}
+
+class StateController {
+    var step: Int = 1
+////    var anchor:Experience.Box!
+//
+//    func updateAnchor(anchor) {
+//        switch step {
+//        case 1:
+//
+//        case 2:
+//        default:
+//            print("nah")
+//        }
+//    }
+    
+    
+
+}
 }
