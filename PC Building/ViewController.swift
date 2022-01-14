b3be526e15ab7dbaa0b980b6837456b1ff601ef7
irenelee <irenelee914@gmail.com>
2022-01-13 22:34:48 -0500

    integrate coreml

--- PC Building/ViewController.swift
+++ PC Building/ViewController.swift
@@ -7,18 +7,42 @@
 
 import UIKit
 import RealityKit
+import ARKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSessionDelegate {
     
     @IBOutlet var arView: ARView!
     var notificationTrigger: RAM.NotificationTrigger!
     var stateController: StateController!
     var textInstructions:UILabel!
     
+    /// Vision request for the detection model
+    private var requests = [VNRequest]()
+    /// Layer used to host detectionOverlay layer
+    var rootLayer: CALayer! = nil
+    /// The detection overlay layer used to render bounding boxes
+    var detectionOverlay: CALayer! = nil
+    /// Whether the current frame should be skipped (in terms of model predictions)
+    var shouldSkipFrame = 0
+    /// How often (in terms of camera frames) should the app run predictions
+    let predictEvery = 3
+    var lastOrientation: CGImagePropertyOrientation = .right
+    var bufferSize: CGSize = .zero
+    
+    private var previewLayer: AVCaptureVideoPreviewLayer! = nil
+    
+    let predictionQueue = DispatchQueue(label: "predictionQueue",
+                                        qos: .userInitiated,
+                                        attributes: [],
+                                        autoreleaseFrequency: .inherit,
+                                        target: nil)
+    
+
     lazy var statusViewController: InstructionsViewController = {
         return children.lazy.compactMap({ $0 as? InstructionsViewController }).first!
     }()
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,15 +52,65 @@ class ViewController: UIViewController {
                     self.view.addSubview(pop)
         }
         
-//        textInstructions = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 20))
-//        textInstructions.center = CGPoint(x: self.view.frame.midX, y: self.view.frame.midY+250)
-//        textInstructions.textAlignment = .center
-//        textInstructions.text = "I'm a test label"
-//        self.view.addSubview(textInstructions)
+        arView.session.delegate = self
+        rootLayer = arView.layer
+        
+        let videoDevice = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: .video, position: .back).devices.first
+        do {
+            try  videoDevice!.lockForConfiguration()
+            let dimensions = CMVideoFormatDescriptionGetDimensions((videoDevice?.activeFormat.formatDescription)!)
+            print(dimensions)
+            print(arView.frame)
+//            bufferSize.width = CGFloat(dimensions.width)
+//            bufferSize.height = CGFloat(dimensions.height)
+            bufferSize.width = CGFloat(arView.frame.width)
+            bufferSize.height = CGFloat(arView.frame.height)
+//            bufferSize.width = 640.0
+//            bufferSize.height = 480.0
+            videoDevice!.unlockForConfiguration()
+        }
+        catch {
+            print(error)
+        }
+        setupLayers()
+        
+        guard let modelURL = Bundle.main.url(forResource: "FullNetwork", withExtension: "mlmodelc") else {
+            print("ERROR")
+            return
+        }
+        do {
+            let visionModel = try VNCoreMLModel(for: MLModel(contentsOf: modelURL))
+            let objectRecognition = VNCoreMLRequest(model: visionModel, completionHandler: { (request, error) in
+                DispatchQueue.main.async(execute: {
+                    // perform all the UI updates on the main queue
+                    if let results = request.results {
+                        print(results)
+                        self.drawVisionRequestResults(results)
+                    }
+                })
+            })
+            self.requests = [objectRecognition]
+        } catch let error as NSError {
+            print("Model loading went wrong: \(error)")
+        }
+//        diceDetectionRequest.imageCropAndScaleOption = .scaleFill
         
         stateController = StateController()
         updateARView()
     }
+    
+    func setupLayers() {
+        self.detectionOverlay = CALayer() // container layer that has all the renderings of the observations
+        self.detectionOverlay.name = "DetectionOverlay"
+        self.detectionOverlay.bounds = CGRect(x: 0.0,
+                                              y: 0.0,
+                                              width: bufferSize.width,
+                                              height: bufferSize.height)
+        self.detectionOverlay.position = CGPoint(x: self.rootLayer.bounds.midX,
+                                                 y: self.rootLayer.bounds.midY)
+        self.rootLayer.addSublayer(self.detectionOverlay)
+    }
+    
     
     @IBAction func prevButton(_ sender: Any) {
         if stateController.step > 0 {
@@ -55,15 +129,16 @@ class ViewController: UIViewController {
     func updateARView() -> Void {
         if stateController.step == 0 {
             statusViewController.showMessage("POINT CAMERA TOWARDS MOTHEROARD")
+            self.detectionOverlay.isHidden = false
         }
         else if stateController.step == 1 {
+            self.detectionOverlay.isHidden = true
             //step 1, depress RAM levers
             arView.scene.anchors.removeAll()
             let anchor = try! RAM.loadRAM1()
             anchor.generateCollisionShapes(recursive: true)
             arView.scene.anchors.append(anchor)
             notificationTrigger = anchor.notifications.stepRAM2
-//            textInstructions.text = "Depress the white levers"
             statusViewController.showMessage("DEPRESS THE WHITE LEVERS")
             
             
@@ -71,25 +146,140 @@ class ViewController: UIViewController {
         else if stateController.step == 2 {
             //step 2, insert RAM sticks
             notificationTrigger.post()
-//            textInstructions.text = "Place ram sticks in until click"
             statusViewController.showMessage("PLACE RAM STICKS IN UNTIL CLICK")
             
         }
 }
 
-class StateController {
-    var step: Int = 0
-////    var anchor:Experience.Box!
-//
-//    func updateAnchor(anchor) {
-//        switch step {
-//        case 1:
-//
-//        case 2:
-//        default:
-//            print("nah")
-//        }
-//    }
+    class StateController {
+        var step: Int = 0
     }
     
+}
+
+
+extension ViewController {
+    
+    func drawVisionRequestResults(_ results: [Any]) {
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        detectionOverlay.sublayers = nil // remove all the old recognized objects
+        for observation in results where observation is VNRecognizedObjectObservation {
+            guard let objectObservation = observation as? VNRecognizedObjectObservation else {
+                continue
+            }
+            // Select only the label with the highest confidence.
+            let topLabelObservation = objectObservation.labels[0]
+            let objectBounds = VNImageRectForNormalizedRect(objectObservation.boundingBox, Int(bufferSize.width), Int(bufferSize.height))
+            
+            let shapeLayer = self.createRoundedRectLayerWithBounds(objectBounds)
+            
+            let textLayer = self.createTextSubLayerInBounds(objectBounds,
+                                                            identifier: topLabelObservation.identifier,
+                                                            confidence: topLabelObservation.confidence)
+            shapeLayer.addSublayer(textLayer)
+            detectionOverlay.addSublayer(shapeLayer)
+        }
+        self.updateLayerGeometry()
+        CATransaction.commit()
+    }
+    
+    func createTextSubLayerInBounds(_ bounds: CGRect, identifier: String, confidence: VNConfidence) -> CATextLayer {
+        let textLayer = CATextLayer()
+        textLayer.name = "Object Label"
+        let formattedString = NSMutableAttributedString(string: String(format: "\(identifier)\nConfidence:  %.2f", confidence))
+        let largeFont = UIFont(name: "Helvetica", size: 24.0)!
+        formattedString.addAttributes([NSAttributedString.Key.font: largeFont], range: NSRange(location: 0, length: identifier.count))
+        textLayer.string = formattedString
+        textLayer.bounds = CGRect(x: 0, y: 0, width: bounds.size.height - 10, height: bounds.size.width - 10)
+        textLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        textLayer.shadowOpacity = 0.7
+        textLayer.shadowOffset = CGSize(width: 2, height: 2)
+        textLayer.foregroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [0.0, 0.0, 0.0, 1.0])
+        textLayer.contentsScale = 2.0 // retina rendering
+        // rotate the layer into screen orientation and scale and mirror
+        textLayer.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: 1.0, y: -1.0))
+        return textLayer
+    }
+    
+    func createRoundedRectLayerWithBounds(_ bounds: CGRect) -> CALayer {
+        let shapeLayer = CALayer()
+        shapeLayer.bounds = bounds
+        shapeLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        shapeLayer.name = "Found Object"
+        shapeLayer.backgroundColor = CGColor(colorSpace: CGColorSpaceCreateDeviceRGB(), components: [1.0, 1.0, 0.2, 0.4])
+        shapeLayer.cornerRadius = 7
+        return shapeLayer
+    }
+    
+    func updateLayerGeometry() {
+        let bounds = rootLayer.bounds
+        var scale: CGFloat
+        
+        let xScale: CGFloat = bounds.size.width / bufferSize.height
+        let yScale: CGFloat = bounds.size.height / bufferSize.width
+        
+        scale = fmax(xScale, yScale)
+        if scale.isInfinite {
+            scale = 1.0
+        }
+        CATransaction.begin()
+        CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+        
+        // rotate the layer into screen orientation and scale and mirror
+        detectionOverlay.setAffineTransform(CGAffineTransform(rotationAngle: CGFloat(.pi / 2.0)).scaledBy(x: scale, y: -scale))
+        // center the layer
+        detectionOverlay.position = CGPoint(x: bounds.midX, y: bounds.midY)
+        
+        CATransaction.commit()
+    }
+    
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        // When rate-limiting predicitons, skip frames to predict every x
+        if shouldSkipFrame > 0 {
+            shouldSkipFrame = (shouldSkipFrame + 1) % predictEvery
+        }
+
+        predictionQueue.async {
+            /// - Tag: MappingOrientation
+            // The frame is always oriented based on the camera sensor,
+            // so in most cases Vision needs to rotate it for the model to work as expected.
+            let orientation = UIDevice.current.orientation
+
+            // The image captured by the camera
+            let image = frame.capturedImage
+
+            let imageOrientation: CGImagePropertyOrientation
+            switch orientation {
+            case .portrait:
+                imageOrientation = .right
+            case .portraitUpsideDown:
+                imageOrientation = .left
+            case .landscapeLeft:
+                imageOrientation = .up
+            case .landscapeRight:
+                imageOrientation = .down
+            case .unknown:
+                print("The device orientation is unknown, the predictions may be affected")
+                fallthrough
+            default:
+                // By default keep the last orientation
+                // This applies for faceUp and faceDown
+                imageOrientation = self.lastOrientation
+            }
+
+
+            /// - Tag: PassingFramesToVision
+
+            // Invoke a VNRequestHandler with that image
+            let handler = VNImageRequestHandler(cvPixelBuffer: image, orientation: imageOrientation, options: [:])
+
+            do {
+                try handler.perform(self.requests)
+            } catch {
+                print("CoreML request failed with error: \(error.localizedDescription)")
+            }
+
+        }
+    }
 }
